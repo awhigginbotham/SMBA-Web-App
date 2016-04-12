4b6314774d821f6edee4eab777b2dcb34192ef34
awhigginbotham <alex.higginbotham@gmail.com>
2016-04-12 07:02:39 -0400

    Begin adding in sessions

--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Alex
- * Date: 4/6/2016
- * Time: 7:46 PM
- *
- *
- */
-//database connection
-require_once(__DIR__.'/db_connection.php');
+//redirect if not logged in
+if(!isset($_SESSION['username'])){
+    header("Location: login.php");
+}
 
-//
+    //database connection
+    require_once(__DIR__.'/db_connection.php');
+
+
 
 
 ?>
 
-<?php require_once(__DIR__.'/header.php');?>
+
 
 
 </body>
